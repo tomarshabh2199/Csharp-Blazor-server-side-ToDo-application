ba9f2bb602c896fa749ab76499f9da7c25748363
Shubham <shutomar@deloitte.com>
2023-03-11 00:24:31 +0530

    initial

---
+++ Data/Models/TaskModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApplication.Data.Models
+{
+    public class TaskModel
+    {
+        public string TaskName { get; set; }
+
+        public bool IsComplete { get; set; }
+    }
+}
